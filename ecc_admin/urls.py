cd8f6aa284a70f5c7dd9be5dfd51aef48871ac28
Alexandre Lira <75650952+Ale-Lira@users.noreply.github.com>
2024-03-03 13:36:19 -0500

    evento atualizado

--- ecc_admin/urls.py
+++ ecc_admin/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', include('contas.urls')),
     path('home/', views.home, name='home'),
     path('evento/cadastro', views.cadastro_evento, name='cadastro_evento'),
+    path('evento/<int:pk>', views.evento_detalhe, name='detalhes_evento'),
 ]
--- ecc_admin/views.py
+++ ecc_admin/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import EventoForm
+from .models import Evento
 @login_required
 def home(request):
     return render(request, 'home.html')
@@ -11,7 +12,11 @@ def cadastro_evento(request):
 
         if form.is_valid():
             evento = form.save()
-            return redirect('evento_detalhe', pk = evento.pk)
+            return redirect('detalhes_evento', pk = evento.pk)
     else: 
         form = EventoForm()
     return render(request, 'cadastro_evento.html', {'form': form})
+
+def evento_detalhe(request, pk):
+    evento = get_object_or_404(Evento, pk = pk)
+    return render(request, 'detalhes_evento.html', {'evento': evento})
